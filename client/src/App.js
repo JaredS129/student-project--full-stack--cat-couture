fc1e6b4e51cbbc822511aca7fd4ac6bdefbf5fc2
JaredStevensonDI <jaredstevenson@developersinstitute.co.nz>
2022-06-13 11:35:12 +1000

    add auth0 to client and protect dashboard page, add isLoading state to app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,6 @@
 import { Routes, Route } from "react-router-dom";
+import { useAuth0 } from "@auth0/auth0-react";
+import ProtectedRoute from "./components/ProtectedRoute";
 import styled from "styled-components";
 import Header from "./components/Header";
 import DashboardPage from "./components/dashboard/DashboardPage";
@@ -16,11 +18,19 @@ const StyledApp = styled.div`
 `;
 
 const App = () => {
+  const { isLoading } = useAuth0();
+
+  if (isLoading) {
+    return <p>Loading...</p>;
+  }
+
   return (
     <StyledApp>
       <Header />
       <Routes>
-        <Route exact path="/dashboard" element={<DashboardPage />} />
+        <Route path="/dashboard" element={<ProtectedRoute />}>
+          <Route path="/dashboard" element={<DashboardPage />} />
+        </Route>
         <Route exact path="/" element={<ProductPage />} />
         <Route path="*" element={<NotFoundPage />} />
       </Routes>
