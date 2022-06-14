c04ddae97d6fa840da5deb84487f97ebb7899f4e
JaredStevensonDI <jaredstevenson@developersinstitute.co.nz>
2022-06-14 20:34:57 +1000

    add pagination with limit and page params in server

--- server/products/product.router.js
+++ server/products/product.router.js
@@ -17,19 +17,29 @@ router.get(
       const { limit, page } = req.query;
 
       const safeLimit = limit ? parseInt(limit) : 10;
-      const safePage = page ? parseInt(page) : 1;
+      const safeOffset = page ? parseInt(page) * 10 - 10 : 0;
 
-      const allProducts = productRepository.getAllProducts();
+      const allProducts = await productRepository.getAllProducts();
 
-      const products = await productRepository.getProducts(safeLimit, safePage);
+      const products = await productRepository.getProducts(
+        safeLimit,
+        safeOffset
+      );
 
       const responseResults = {
         products,
-        currentPages: safePage,
+        currentPage: parseInt(page),
         itemsPerPage: safeLimit,
         totalItems: allProducts.length,
         totalPages: Math.ceil(allProducts.length / safeLimit),
       };
+
+      console.log(responseResults.products);
+
+      if (products.length === 0) {
+        console.log("hi");
+        return res.status(404).json({ message: "page not found" });
+      }
 
       return res.json(responseResults);
     } catch (err) {
