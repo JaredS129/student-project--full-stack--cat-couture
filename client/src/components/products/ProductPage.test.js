f61988e9aabc85f51bbafcbb2cf9f558742dbc13
JaredStevensonDI <jaredstevenson@developersinstitute.co.nz>
2022-06-16 14:34:54 +1000

    implement msw integrattion tests for pagination features

--- client/src/components/products/ProductPage.test.js
+++ client/src/components/products/ProductPage.test.js
@@ -13,34 +13,59 @@ beforeEach(async () => {
 
 describe("ProductPage", () => {
   test("WHEN a user goes to the Products page, THEN the pagination control will be displayed", () => {
-    const previousPageButton = screen.getByRole("button", {
+    const previousPageButton = screen.getAllByRole("button", {
       name: "Previous page",
     });
-    const nextPageButton = screen.getByRole("button", { name: "Next page" });
-    expect(previousPageButton).toBeInTheDocument();
-    expect(nextPageButton).toBeInTheDocument();
+    const nextPageButton = screen.getAllByRole("button", { name: "Next page" });
+    previousPageButton.forEach((button) => {
+      expect(button).toBeInTheDocument();
+    });
+    nextPageButton.forEach((button) => {
+      expect(button).toBeInTheDocument();
+    });
   });
 
   test("WHEN a user navigates to the first page of the Products page, THEN the previous/back button of the pagination control will be disabled", () => {
-    const previousPageButton = screen.getByRole("button", {
+    const previousPageButton = screen.getAllByRole("button", {
       name: "Previous page",
     });
-    expect(previousPageButton).toBeDisabled();
+    previousPageButton.forEach((button) => {
+      expect(button).toBeDisabled();
+    });
   });
 
-  test.todo(
-    "WHEN the user navigates to the second page of the Products page, THEN the previous/back button of the pagination control will be enabled"
-  );
+  test("WHEN the user navigates to the second page of the Products page, THEN the previous/back button of the pagination control will be enabled", () => {
+    const previousPageButton = screen.getAllByRole("button", {
+      name: "Previous page",
+    });
+    const nextPageButton = screen.getAllByRole("button", {
+      name: "Next page",
+    });
+    userEvent.click(nextPageButton[0]);
+    previousPageButton.forEach((button) => {
+      expect(button).toBeEnabled();
+    });
+  });
 
-  test.todo(
-    "WHEN the user navigates to the last page of the Products page, THEN next button of the pagination control will be disabled"
-  );
+  test("WHEN the user navigates to the last page of the Products page, THEN next button of the pagination control will be disabled", () => {
+    const nextPageButton = screen.getAllByRole("button", {
+      name: "Next page",
+    });
+    userEvent.click(nextPageButton[0]);
+    nextPageButton.forEach((button) => {
+      expect(button).toBeDisabled();
+    });
+  });
 
   test("WHEN a user goes to the Products page, THEN the current page will be highlighted in the pagination control", () => {
-    const pageDisplay = screen.queryByText(/page/i);
-    expect(pageDisplay.textContent).toBe("Page 1 of 2");
-    const nextPageButton = screen.getByRole("button", { name: "Next page" });
-    userEvent.click(nextPageButton);
-    expect(pageDisplay.textContent).toBe("Page 2 of 2");
+    const pageDisplay = screen.queryAllByText(/page/i);
+    pageDisplay.forEach((text) => {
+      expect(text.textContent).toBe("Page 1 of 2");
+    });
+    const nextPageButton = screen.getAllByRole("button", { name: "Next page" });
+    userEvent.click(nextPageButton[0]);
+    pageDisplay.forEach((text) => {
+      expect(text.textContent).toBe("Page 2 of 2");
+    });
   });
 });
